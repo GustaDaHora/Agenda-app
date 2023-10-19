10f44e766223bd308629e2e6c0819104e836a773
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-10-18 21:36:56 -0400

    Update styles.js
    Changed universal styles

--- src/styles.js
+++ src/styles.js
@@ -6,11 +6,13 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     background: teal;
     color: whitesmoke;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   }
 
-  h1, h2, h3 {  
-    -webkit-text-stroke: 1px black;
+  h1, h2, h3 {
+    margin: 0;
+    padding: 1rem;
+    font-size: 2rem;
   }
 `;
 
