ab33ed1f93b46415f9ce9b842ab3bb875a1e50d8
Gustavo <106494958+GustaDaHora@users.noreply.github.com>
2023-10-24 21:26:41 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,8 +13,9 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "../assets/icon.ico",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js')
     },
   });
 
